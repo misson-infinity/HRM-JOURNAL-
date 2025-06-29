2e87828976abcad5e5989ab3cba5042be3fd6f5a
misson-infinity <shortstoriesbgc25@gmail.com>
2025-06-29 20:37:19 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'infinity-tracker-pro-v6';
+const CACHE_NAME = 'infinity-x-v1';
 const urlsToCache = [ '/', 'index.html', 'transactions.html', 'reports.html', 'budgets.html', 'settings.html', 'developer.html', 'style.css', 'main.js', 'ui.js', 'manifest.json', 'logo.svg', 'vector_lecture_design.png', 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css', 'https://cdn.jsdelivr.net/npm/chart.js' ];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
