13b080081f4b384a3173263784eda4aee9aa9eb1
misson-infinity <shortstoriesbgc25@gmail.com>
2025-07-04 20:18:28 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,9 @@
 // config.js
 const CONFIG = {
-    APP_NAME: "I Expanse Tracker",
-    APP_SLOGAN: "Infinity in Control, Clarity in Spending",
-    DEVELOPER_NAME: "Md Habibur Rahman Mahi",
-    DEVELOPER_TITLE: "Founder and CEO of Infinity Group",
-    DEVELOPER_PHOTO: "Picsart_24-12-22_22-58-18-749.png",
-    APP_LOGO: "image (4).png",
-    CONTACT: {
-        WHATSAPP: "01727722018",
-        TELEGRAM: "01727722018",
-        INSTAGRAM: "https://www.instagram.com/h.r_mahi_?igsh=Z242dWFtcDZwdjF2",
-        FACEBOOK: "https://www.facebook.com/share/1L8yaf25bk/"
-    },
+    APP_NAME: "I Expanse Tracker", APP_SLOGAN: "Infinity in Control, Clarity in Spending",
+    DEVELOPER_NAME: "Md Habibur Rahman Mahi", DEVELOPER_TITLE: "Founder and CEO of Infinity Group",
+    DEVELOPER_PHOTO: "Picsart_24-12-22_22-58-18-749.png", APP_LOGO: "image (4).png",
+    CONTACT: { WHATSAPP: "01727722018", TELEGRAM: "01727722018", INSTAGRAM: "https://www.instagram.com/h.r_mahi_?igsh=Z242dWFtcDZwdjF2", FACEBOOK: "https://www.facebook.com/share/1L8yaf25bk/" },
     DEFAULT_CURRENCY: "৳",
     INITIAL_CATEGORIES: {
         income: [ { id: 'cat_inc_1', name: 'Salary', icon: 'briefcase-alt-2' }, { id: 'cat_inc_2', name: 'Business', icon: 'store-alt' }, { id: 'cat_inc_3', name: 'Gifts', icon: 'gift' }, { id: 'cat_inc_4', name: 'Other', icon: 'plus-circle' } ],
