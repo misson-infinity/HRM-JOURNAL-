1c312ab4369d48c4e3e559f65026eb81b5ac84b9
misson-infinity <shortstoriesbgc25@gmail.com>
2025-07-04 19:33:30 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+// config.js
+const CONFIG = {
+    APP_NAME: "I Expanse Tracker",
+    APP_SLOGAN: "Infinity in Control, Clarity in Spending",
+    DEVELOPER_NAME: "Md Habibur Rahman Mahi",
+    DEVELOPER_TITLE: "Founder and CEO of Infinity Group",
+    DEVELOPER_PHOTO: "Picsart_24-12-22_22-58-18-749.png",
+    APP_LOGO: "image (4).png",
+    CONTACT: {
+        WHATSAPP: "01727722018",
+        TELEGRAM: "01727722018",
+        INSTAGRAM: "https://www.instagram.com/h.r_mahi_?igsh=Z242dWFtcDZwdjF2",
+        FACEBOOK: "https://www.facebook.com/share/1L8yaf25bk/"
+    },
+    DEFAULT_CURRENCY: "৳",
+    INITIAL_CATEGORIES: {
+        income: [
+            { id: 'cat_inc_1', name: 'Salary', icon: 'briefcase' },
+            { id: 'cat_inc_2', name: 'Business', icon: 'store' },
+            { id: 'cat_inc_3', name: 'Gifts', icon: 'gift' },
+            { id: 'cat_inc_4', name: 'Other', icon: 'plus-circle' }
+        ],
+        expense: [
+            { id: 'cat_exp_1', name: 'Food & Drinks', icon: 'utensils-crossed' },
+            { id: 'cat_exp_2', name: 'Shopping', icon: 'shopping-bag' },
+            { id: 'cat_exp_3', name: 'Transport', icon: 'bus' },
+            { id: 'cat_exp_4', name: 'Bills & Fees', icon: 'receipt' },
+            { id: 'cat_exp_5', name: 'Entertainment', icon: 'popcorn' },
+            { id: 'cat_exp_6', name: 'Health', icon: 'siren' },
+            { id: 'cat_exp_7', name: 'Education', icon: 'graduation-cap' },
+            { id: 'cat_exp_8', name: 'Other', icon: 'more-horizontal' }
+        ]
+    }
+};
